aeb0e0137007e14d889c9cf2ad0effccdc490640
Sonmoham <dnddl505@gmail.com>
2022-11-24 15:45:29 +0900

    [Chore] Data - empty UDFavoriteProductUseCase 파일 추가

---
+++ SimpleMall/Sources/Data/Persistent/UserDefaults/UseCases/UDFavoriteProductUseCase.swift
@@ -0,0 +1,27 @@
+//
+//  UDFavoriteProductUseCase.swift
+//  SimpleMall
+//
+//  Created by Son Daehong on 2022/11/24.
+//
+
+import Foundation
+
+/// Persistent - UserDefaults FavoriteProductUseCase
+final class UDFavoriteProductUseCase { }
+
+/* TODO: 채택, 준수 구현
+extension UDFavoriteProductUseCase: FavoriteProductUseCase {
+    func products() -> RxSwift.Observable<[Product]> {
+        <#code#>
+    }
+    
+    func save(product: Product) -> RxSwift.Observable<Void> {
+        <#code#>
+    }
+    
+    func delete(product: Product) -> RxSwift.Observable<Void> {
+        <#code#>
+    }
+}
+*/
