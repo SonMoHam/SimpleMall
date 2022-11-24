b5b8274ca9667c30e4d8353fb07d2b4ef818067f
Sonmoham <dnddl505@gmail.com>
2022-11-24 17:59:00 +0900

    [Chore] Data Endpoints - empty HomeEndpoints 추가

---
+++ SimpleMall/Sources/Data/Network/Endpoints/HomeEndpoints.swift
@@ -0,0 +1,13 @@
+//
+//  HomeEndpoints.swift
+//  SimpleMall
+//
+//  Created by Son Daehong on 2022/11/24.
+//
+
+import Foundation
+
+struct HomeEndpoints {
+    // getHome
+    // getProducts()
+}
