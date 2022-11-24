128dbe63c7e6e45d6217d21b9f50026a198cc60b
Sonmoham <dnddl505@gmail.com>
2022-11-24 21:30:58 +0900

    [Refactor] HomeEndpoints - baseURL 이동

--- SimpleMall/Sources/Data/Network/CustomLayer/Endpoints/HomeEndpoints.swift
+++ SimpleMall/Sources/Data/Network/CustomLayer/Endpoints/HomeEndpoints.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 
-// TODO: 이동
-let baseURL = "http://d2bab9i9pr8lds.cloudfront.net/api"
-
 struct HomeEndpoints {
     static func getHome() -> Endpoint<HomeResponseDTO> {
-        return Endpoint(baseURL: baseURL, path: "/home", method: .get)
+        return Endpoint(
+            baseURL: "http://d2bab9i9pr8lds.cloudfront.net/api",
+            path: "/home",
+            method: .get)
     }
     
     static func getGoods(lastId: Int) -> Endpoint<[Product]> {
-        return Endpoint(baseURL: baseURL, path: "/home/goods", method: .get)
+        return Endpoint(
+            baseURL: "http://d2bab9i9pr8lds.cloudfront.net/api",
+            path: "/home/goods",
+            method: .get)
     }
-    // getProducts()
 }
