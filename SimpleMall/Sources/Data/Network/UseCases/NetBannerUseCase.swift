3e984f079811815103f50160c2c32253bf3b604d
Sonmoham <dnddl505@gmail.com>
2022-11-24 15:55:07 +0900

    [Chore] Data - empty 네트워크 유스케이스 (BannerUseCase, ProductUseCase) 추가

---
+++ SimpleMall/Sources/Data/Network/UseCases/NetBannerUseCase.swift
@@ -0,0 +1,20 @@
+//
+//  NetBannerUseCase.swift
+//  SimpleMall
+//
+//  Created by Son Daehong on 2022/11/24.
+//
+
+import Foundation
+import RxSwift
+
+/// Network - BannerUseCase 구현체
+final class NetBannerUseCase { }
+
+/* TODO: 채택, 준수 구현
+extension NetBannerUseCase: BannerUseCase {
+    func banners() -> Observable<[Banner]> {
+        <#code#>
+    }
+}
+*/
---
+++ SimpleMall/Sources/Data/Network/UseCases/NetProductUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  NetProductUseCase.swift
+//  SimpleMall
+//
+//  Created by Son Daehong on 2022/11/24.
+//
+
+import Foundation
+import RxSwift
+
+/// Network - ProductUseCase 구현체
+final class NetProductUseCase { }
+
+/* TODO: 채택, 준수 구현
+extension NetProductUseCase: ProductUseCase {
+    func products() -> RxSwift.Observable<[Product]> {
+        <#code#>
+    }
+    
+    func products(lastID: Int) -> RxSwift.Observable<[Product]> {
+        <#code#>
+    }
+}
+ */
