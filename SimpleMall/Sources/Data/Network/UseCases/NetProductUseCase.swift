b28f06c337d4bec4d098be9de0d2e9aef223eb3e
Sonmoham <dnddl505@gmail.com>
2022-11-24 16:07:13 +0900

    [Test] Data - StubProductUseCase 추가

--- SimpleMall/Sources/Data/Network/UseCases/NetProductUseCase.swift
+++ SimpleMall/Sources/Data/Network/UseCases/NetProductUseCase.swift
@@ -22,3 +22,59 @@ extension NetProductUseCase: ProductUseCase {
     }
 }
  */
+
+final class StubProductUseCase: ProductUseCase {
+    func products() -> Observable<[Product]> {
+        let dummyProducts: [Product] = [
+            Product(
+                id: 1,
+                name: "first",
+                imageURL: "list.bullet",
+                actualPrice: 100,
+                price: 10,
+                isNew: true,
+                sellCount: 9),
+            Product(
+                id: 2,
+                name: "second",
+                imageURL: "list.bullet",
+                actualPrice: 200,
+                price: 20,
+                isNew: true,
+                sellCount: 10)
+        ]
+        return Observable.create { observer -> Disposable in
+            DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+                observer.onNext(dummyProducts)
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func products(lastID: Int) -> Observable<[Product]> {
+        let dummyNextProducts: [Product] = [
+            Product(
+                id: 3,
+                name: "third",
+                imageURL: "person.fill",
+                actualPrice: 300,
+                price: 30,
+                isNew: false,
+                sellCount: 8),
+            Product(
+                id: 4,
+                name: "fourth",
+                imageURL: "person.fill",
+                actualPrice: 400,
+                price: 40,
+                isNew: false,
+                sellCount: 12)
+        ]
+        return Observable.create { observer -> Disposable in
+            DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+                observer.onNext(dummyNextProducts)
+            }
+            return Disposables.create()
+        }
+    }
+}
