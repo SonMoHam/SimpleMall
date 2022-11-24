1e4ba4b3395a17680a5ea5f168dabb97325c0285
Sonmoham <dnddl505@gmail.com>
2022-11-24 18:07:02 +0900

    [Feat] Data Network - HomeEndpoints (getHome, getGoods) 추가

--- SimpleMall/Sources/Data/Network/Endpoints/HomeEndpoints.swift
+++ SimpleMall/Sources/Data/Network/Endpoints/HomeEndpoints.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 struct HomeEndpoints {
-    // getHome
+    static func getHome() -> Endpoint<HomeResponseDTO> {
+        return Endpoint(baseURL: "", path: "/home", method: .get)
+    }
+    
+    static func getGoods(lastId: Int) -> Endpoint<[Product]> {
+        return Endpoint(baseURL: "", path: "/home/goods", method: .get)
+    }
     // getProducts()
 }
