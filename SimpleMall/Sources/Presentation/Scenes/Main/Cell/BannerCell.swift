30c605e5e2c32947ed9c4e4214534cb8add0a8c5
Sonmoham <dnddl505@gmail.com>
2022-11-25 16:38:34 +0900

    [Test] Banner&Product Cell - 컬렉션뷰 레이아웃 확인 용 샘플 코드 추가

--- SimpleMall/Sources/Presentation/Scenes/Main/Cell/BannerCell.swift
+++ SimpleMall/Sources/Presentation/Scenes/Main/Cell/BannerCell.swift
@@ -8,6 +8,28 @@
 import Foundation
 import UIKit
 
+import SnapKit
+
 final class BannerCell: UICollectionViewCell {
+    let imageView: UIImageView = UIImageView()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(imageView)
+        imageView.image = UIImage(systemName: "heart")
+        self.backgroundColor = .red
+        setupConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.height.equalTo(200)
+        }
+    }
 }
--- SimpleMall/Sources/Presentation/Scenes/Main/Cell/ProductCell.swift
+++ SimpleMall/Sources/Presentation/Scenes/Main/Cell/ProductCell.swift
@@ -8,6 +8,44 @@
 import Foundation
 import UIKit
 
+import SnapKit
+
 final class ProductCell: UICollectionViewCell {
+    let container: UIView = UIView()
+    let imageView: UIImageView = UIImageView()
+    let detailContainer: UIView = UIView()
+    let priceLabel: UILabel = UILabel()
+    let descriptionLabel: UILabel = UILabel()
+    let sellCountLabel: UILabel = UILabel()
+    let isNewLabel: UILabel = {
+       let label = UILabel()
+        label.text = "NEW"
+        label.layer.borderWidth = 1
+        label.layer.borderColor = UIColor.black.cgColor
+        return label
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(container)
+        container.addSubview(isNewLabel)
+        container.backgroundColor = .yellow
+        setupConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        container.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.height.equalTo(100)
+        }
+        
+        isNewLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
