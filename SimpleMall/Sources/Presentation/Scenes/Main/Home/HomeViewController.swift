aebb726353f60a1d527cb48bbf511d7c4a653aff
Sonmoham <dnddl505@gmail.com>
2022-11-25 16:40:18 +0900

    [Feat] HomeViewController - 컬렉션뷰 컴포지셔널 레이아웃 추가

--- SimpleMall/Sources/Presentation/Scenes/Main/Home/HomeViewController.swift
+++ SimpleMall/Sources/Presentation/Scenes/Main/Home/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 import SnapKit
 import RxSwift
+import RxCocoa
 
 final class HomeViewController: UIViewController {
     
@@ -34,10 +35,17 @@ final class HomeViewController: UIViewController {
     private lazy var collectionView: UICollectionView = {
         let collectionView = UICollectionView(
             frame: .zero,
-            collectionViewLayout: UICollectionViewFlowLayout())
+            collectionViewLayout: collectionViewLayout())
         collectionView.backgroundColor = .gray
+        collectionView.register(
+            BannerCell.self,
+            forCellWithReuseIdentifier: BannerCell.reuseIdentifier)
+        collectionView.register(
+            ProductCell.self,
+            forCellWithReuseIdentifier: ProductCell.reuseIdentifier)
         return collectionView
     }()
+    
     
     // MARK: Initializing
     
@@ -57,6 +65,7 @@ final class HomeViewController: UIViewController {
         self.view.backgroundColor = .brown
         self.view.addSubview(collectionView)
         setupConstraints()
+
     }
     
     // MARK: Methods
@@ -65,5 +74,31 @@ final class HomeViewController: UIViewController {
         collectionView.snp.makeConstraints { make in
             make.edges.equalTo(view.safeAreaLayoutGuide).inset(30)
         }
+    }
+    
+    
+    private func collectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
+            guard let section = CollectionViewSection(rawValue: sectionIndex) else {
+                return nil
+            }
+
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),  // group 내 item, width 꽉 채움
+                heightDimension: .estimated(100))   // 동적 높이
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),  // width 꽉 채움
+                heightDimension: .estimated(100))   // 동적 높이
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: groupSize,
+                subitem: item,
+                count: section.columnCount)
+            let layoutSection = NSCollectionLayoutSection(group: group)
+            layoutSection.orthogonalScrollingBehavior = section.orthogonalScrollingBehavior
+            return layoutSection
+        }
+        return layout
     }
 }
