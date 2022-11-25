dfc4ce1e4ebe4bac5d42ddbe9189a9d3c893bd43
Sonmoham <dnddl505@gmail.com>
2022-11-25 12:22:45 +0900

    [Feat] 추가 empty BannerCell

---
+++ SimpleMall/Sources/Presentation/Scenes/Main/Cell/BannerCell.swift
@@ -0,0 +1,13 @@
+//
+//  BannerCell.swift
+//  SimpleMall
+//
+//  Created by Son Daehong on 2022/11/25.
+//
+
+import Foundation
+import UIKit
+
+final class BannerCell: UICollectionViewCell {
+    
+}
