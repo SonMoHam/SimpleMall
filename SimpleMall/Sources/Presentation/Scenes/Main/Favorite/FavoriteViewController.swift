e2f0598e8abcfe26b9f29a5bd1f8454bfd235d12
Sonmoham <dnddl505@gmail.com>
2022-11-27 02:59:58 +0900

    [Feat] FavoriteViewController - 레이아웃 구현

--- SimpleMall/Sources/Presentation/Scenes/Main/Favorite/FavoriteViewController.swift
+++ SimpleMall/Sources/Presentation/Scenes/Main/Favorite/FavoriteViewController.swift
@@ -8,7 +8,45 @@
 import Foundation
 import UIKit
 
+import SnapKit
+import RxSwift
+import RxCocoa
+
 final class FavoriteViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    private lazy var collectionView: UICollectionView = {
+       let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: collectionViewLayout())
+        collectionView.backgroundColor = .white
+        collectionView.register(
+            ProductCell.self,
+            forCellWithReuseIdentifier: ProductCell.reuseIdentifier)
+        return collectionView
+    }()
+    
+    typealias DataSource = UICollectionViewDiffableDataSource
+    private lazy var dataSource: DataSource<Int, Product> = DataSource(
+        collectionView: collectionView,
+        cellProvider: { [weak self] collectionView, indexPath, element in
+            if let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: ProductCell.reuseIdentifier,
+                for: indexPath
+            ) as? ProductCell {
+                cell.configure(element)
+                cell.favoriteButton.isHidden = true
+                cell.delegate = self
+                return cell
+            } else {
+                return UICollectionViewCell()
+            }
+        }
+    )
+    
+    private var snapshot = NSDiffableDataSourceSnapshot<Int, Product>()
+    var disposeBag = DisposeBag()
     
     // MARK: Initializing
     
@@ -26,6 +64,61 @@ final class FavoriteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .yellow
+        self.view.addSubview(collectionView)
+        setupConstraints()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#function)
+        
+        self.snapshot.deleteAllItems()
+        
+        let decoder = JSONDecoder()
+        guard let data = UserDefaults.Favorite.object(forKey: .products) as? Data,
+              let products = try? decoder.decode([Product].self, from: data)
+        else { return }
+        self.snapshot.appendSections([0])
+        self.snapshot.appendItems(products)
+        self.dataSource.apply(self.snapshot, animatingDifferences: true)
+        
+    }
+    
+    // MARK: Methods
+    
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func collectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
+
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),  // group 내 item, width 꽉 채움
+                heightDimension: .estimated(10))   // 동적 높이
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),  // width 꽉 채움
+                heightDimension: .estimated(10))   // 동적 높이
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: groupSize,
+                subitem: item,
+                count: 1)
+            let layoutSection = NSCollectionLayoutSection(group: group)
+            layoutSection.orthogonalScrollingBehavior = .none
+            return layoutSection
+        }
+        return layout
+    }
+}
+
+// MARK: - ProductCellDelegate
+
+extension FavoriteViewController: ProductCellDelegate {
+    func favoriteButtonDidTapped(_ isFavorite: Bool, id: Int) {
+        print(isFavorite,id)
     }
 }
