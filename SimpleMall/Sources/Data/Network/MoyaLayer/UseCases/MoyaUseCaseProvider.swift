274515b9a9b7f2d2870eb86a16ea89a3bf565e1b
Sonmoham <dnddl505@gmail.com>
2022-11-26 18:58:10 +0900

    [Feat] Moya UseCaes 추가

---
+++ SimpleMall/Sources/Data/Network/MoyaLayer/UseCases/MoyaUseCaseProvider.swift
@@ -0,0 +1,30 @@
+//
+//  MoyaUseCaseProvider.swift
+//  SimpleMall
+//
+//  Created by 손대홍 on 2022/11/26.
+//
+
+import Foundation
+
+final class MoyaUseCaseProvider {
+    private let networkService: HomeAPI
+    
+    init() {
+        self.networkService = HomeAPI()
+    }
+}
+
+extension MoyaUseCaseProvider: UseCaseProdiver {
+    func makeBannerUseCase() -> BannerUseCase {
+        return MoyaBannerUseCase(networkService: networkService)
+    }
+    
+    func makeProductUseCase() -> ProductUseCase {
+        return MoyaProductUseCase(networkService: networkService)
+    }
+    
+    func makeFavoriteProductUseCase() -> FavoriteProductUseCase {
+        return StubFavoriteProductUseCase()
+    }
+}
