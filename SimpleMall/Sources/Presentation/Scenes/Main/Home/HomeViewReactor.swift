3f043ffd1fa0231869835fe55406575980878c1e
Sonmoham <dnddl505@gmail.com>
2022-11-26 19:39:53 +0900

    [Feat] HomeViewReactor 로직 구현

--- SimpleMall/Sources/Presentation/Scenes/Main/Home/HomeViewReactor.swift
+++ SimpleMall/Sources/Presentation/Scenes/Main/Home/HomeViewReactor.swift
@@ -6,3 +6,94 @@
 //
 
 import Foundation
+
+import ReactorKit
+import RxSwift
+
+final class HomeViewReactor: Reactor {
+    enum Action {
+        case refresh
+        case pagination(
+            contentHeight: CGFloat,
+            contentOffsetY: CGFloat,
+            scrollViewHeight: CGFloat
+        )
+        case didTapFavorite(goodsID: Int, isFavorite: Bool)
+    }
+    
+    enum Mutation {
+        case refreshItems([Banner], [Product])
+        case appendNextProducts([Product])
+        case updateFavoriteProducts(Product, Bool)
+    }
+    
+    struct State {
+        var banners: [Banner]
+        var products: [Product]
+        var isRefresh: Bool = false
+    }
+    
+    let initialState: State
+    
+    private let bannerUseCase: BannerUseCase
+    private let productUseCase: ProductUseCase
+    private let favoriteProductUseCase: FavoriteProductUseCase
+    
+    init(
+        bannerUseCase: BannerUseCase,
+        productUseCase: ProductUseCase,
+        favoriteProductUseCase: FavoriteProductUseCase
+    ) {
+        self.bannerUseCase = bannerUseCase
+        self.productUseCase = productUseCase
+        self.favoriteProductUseCase = favoriteProductUseCase
+        self.initialState = State(banners: [], products: [])
+    }
+    
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .refresh:
+            return Observable
+                .combineLatest(bannerUseCase.banners(), productUseCase.products())
+                .map { Mutation.refreshItems($0, $1) }
+            
+        case let .pagination(contentHeight, contentOffsetY, scrollViewHeight):
+            if contentHeight - scrollViewHeight < contentOffsetY,
+               let lastID = currentState.products.last?.id
+            {
+                return productUseCase
+                    .products(lastID: lastID)
+                    .map { Mutation.appendNextProducts($0) }
+            } else {
+                return .empty()
+            }
+        case let .didTapFavorite(goodsID, isFavorite):
+            // TODO: 찜 관련 로직 작성
+            if let product = currentState.products.filter({ $0.id == goodsID }).first {
+                return .just(.updateFavoriteProducts(product, isFavorite))
+            } else {
+                return .empty()
+            }
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+        case let .refreshItems(banners, products):
+            newState.banners = banners
+            newState.products = products
+            newState.isRefresh = true
+        case .appendNextProducts(let products):
+            let newProducts = state.products + products
+            newState.products = newProducts
+            newState.isRefresh = false
+        case let .updateFavoriteProducts(product, isFavorite):
+            break
+        }
+        
+        return newState
+    }
+}
