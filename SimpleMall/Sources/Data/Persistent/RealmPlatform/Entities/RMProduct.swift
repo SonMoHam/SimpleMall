6e8652c8ecf26d8f7458e0b8e1600808eaaaa753
Sonmoham <dnddl505@gmail.com>
2022-11-28 00:01:12 +0900

    [Feat] Domain & Realm Product 엔티티 Convertible 채택, 준수

--- SimpleMall/Sources/Data/Persistent/RealmPlatform/Entities/RMProduct.swift
+++ SimpleMall/Sources/Data/Persistent/RealmPlatform/Entities/RMProduct.swift
@@ -22,3 +22,32 @@ final class RMProduct: Object {
         return "id"
     }
 }
+
+extension RMProduct: DomainConvertible {
+    func toDomain() -> Product {
+        return Product(
+            id: id,
+            name: name,
+            imageURL: imageURL,
+            actualPrice: actualPrice,
+            price: price,
+            isNew: isNew,
+            sellCount: sellCount,
+            isFavorite: isFavorite)
+    }
+}
+
+extension Product: RealmConvertible {
+    func toRealm() -> RMProduct {
+        let object = RMProduct()
+        object.id = id
+        object.name = name
+        object.imageURL = imageURL
+        object.actualPrice = actualPrice
+        object.price = price
+        object.isNew = isNew
+        object.sellCount = sellCount
+        object.isFavorite = isFavorite ?? false
+        return object
+    }
+}
