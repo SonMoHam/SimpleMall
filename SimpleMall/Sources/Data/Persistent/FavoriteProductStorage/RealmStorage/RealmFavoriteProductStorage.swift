6346e31608534a99088bebd619916b1e7c4bc5be
Sonmoham <dnddl505@gmail.com>
2022-11-27 23:05:20 +0900

    Add empty RealmFavoriteProductStorage file

---
+++ SimpleMall/Sources/Data/Persistent/FavoriteProductStorage/RealmStorage/RealmFavoriteProductStorage.swift
@@ -0,0 +1,28 @@
+//
+//  RealmFavoriteProductStorage.swift
+//  SimpleMall
+//
+//  Created by 손대홍 on 2022/11/27.
+//
+
+import Foundation
+
+final class RealmFavoriteProductStorage { }
+
+/*
+extension RealmFavoriteProductStorage: FavoriteProductStorage {
+    func products() -> Result<[Product], Error> {
+        <#code#>
+    }
+    
+    func save(product: Product) -> Result<Void, Error> {
+        <#code#>
+    }
+    
+    func delete(product: Product) -> Result<Void, Error> {
+        <#code#>
+    }
+    
+    
+}
+*/
