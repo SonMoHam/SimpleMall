25fd3bf441992dab8b79b1234aa782ddb9aff0bd
Sonmoham <dnddl505@gmail.com>
2022-11-27 17:25:10 +0900

    [Feat] UserDefaultsProductUseCase 구현

--- SimpleMall/Sources/Data/Persistent/FavoriteProductStorage/UserDefaultsStorage/UDFavoriteProductUseCase.swift
+++ SimpleMall/Sources/Data/Persistent/FavoriteProductStorage/UserDefaultsStorage/UDFavoriteProductUseCase.swift
@@ -9,23 +9,57 @@ import Foundation
 import RxSwift
 
 /// Persistent - UserDefaults FavoriteProductUseCase
-final class UDFavoriteProductUseCase { }
-
-/* TODO: 채택, 준수 구현
-extension UDFavoriteProductUseCase: FavoriteProductUseCase {
-    func products() -> RxSwift.Observable<[Product]> {
-        <#code#>
-    }
-    
-    func save(product: Product) -> RxSwift.Observable<Void> {
-        <#code#>
-    }
-    
-    func delete(product: Product) -> RxSwift.Observable<Void> {
-        <#code#>
+final class UDFavoriteProductUseCase {
+    private let storage: FavoriteProductStorage
+    init() {
+        storage = UserDefaultsFavoriteProductStorage()
     }
 }
-*/
+
+
+extension UDFavoriteProductUseCase: FavoriteProductUseCase {
+    func products() -> Observable<[Product]> {
+        let result = storage.products()
+        return Observable.create { observer -> Disposable in
+            switch result {
+            case .success(let products):
+                observer.onNext(products)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func save(product: Product) -> Observable<Void> {
+        let result = storage.save(product: product)
+        return Observable.create { observer -> Disposable in
+            switch result {
+            case .success:
+                observer.onNext(())
+            case .failure(let error):
+                print(error.localizedDescription)
+                observer.onNext(())
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func delete(product: Product) -> Observable<Void> {
+        let result = storage.delete(product: product)
+        return Observable.create { observer -> Disposable in
+            switch result {
+            case .success:
+                observer.onNext(())
+            case .failure(let error):
+                print(error.localizedDescription)
+                observer.onNext(())
+            }
+            return Disposables.create()
+        }
+    }
+}
+
 
 final class StubFavoriteProductUseCase: FavoriteProductUseCase {
     func products() -> Observable<[Product]> {
